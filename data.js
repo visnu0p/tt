d9e928cbc98104318875ed91d340934a6de9b38a
visnu0p <104520802+visnu0p@users.noreply.github.com>
2022-06-08 22:32:30 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    mongoLink:'',//database link
+    
+    bot_token:'5265424629:AAH05mayiS3Q2dSXdvTosXGt7eBxGWhxU7g',//bot token
+    
+    bot_name:'New1trybot',//bot username without @
+    
+    bot_admin:1872691186,// admin telegram id
+    
+    channelsList : ['@AirdropVictor'],// channels list
+    
+    reffer_bonus:5000,// refferal bonus amount
+    
+    min_wd:10000,//min withdrawal amount
+    
+    daily_bonus:1000,// daily bonus amount
+    
+    currency:'', //bot cuurency
+    contractadres :  '', //contracr address of coin
+    privateKey : '', //private key of coin
+
+    payment_channel:'@payoutproof12'// payment channel username
+    
+    }
